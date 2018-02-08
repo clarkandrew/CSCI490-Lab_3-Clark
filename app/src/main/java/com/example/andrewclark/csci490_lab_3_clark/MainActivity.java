8412795015ac46a5d7c9f67a85bb2b1792699eb5
BuildTools <unconfigured@null.spigotmc.org>
2018-02-08 18:07:03 -0500

    work update

---
+++ app/src/main/java/com/example/andrewclark/csci490_lab_3_clark/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.andrewclark.csci490_lab_3_clark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final ListView classSched = findViewById(R.id.class_schedule);
+
+
+        String[] classes = new String[] {
+                "CSCI-221",
+                "CSCI-490",
+                "Anthropology",
+                "History",
+                "Descrete Structures"
+        };
+
+        ArrayAdapter<String> arrAdapt =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, classes);
+
+        classSched.setAdapter(arrAdapt);
+
+
+        classSched.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String item = ((TextView)view).getText().toString();
+
+                Toast.makeText(getBaseContext(), item, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        classSched.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = ((TextView)view).getText().toString();
+
+                Toast.makeText(getBaseContext(), "Long Click: " + item, Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+
+
+
+    }
+
+
+
+}
